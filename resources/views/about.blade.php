094d5d5efcbd9080fbea21227b61a9809ae8982c
hf314 <mail.ankan@protonmail.com>
2022-03-29 11:25:06 +0200

    Connecting to css

---
+++ resources/views/about.blade.php
@@ -0,0 +1,11 @@
+<!<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <link rel="stylesheet" href="{{ asset('/css/app.css') }}" type="text/css">
+    <title>Coza Store - About</title>
+</head>
+<body>
+<h1>About</h1>
+</body>
+</html>
